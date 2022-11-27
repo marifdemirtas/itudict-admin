14a7841d8d085d358f10cd497832c1479a3ccef8
barisemremise <barisemremise@hotmail.com>
2022-11-27 21:11:47 +0300

    ADD: creating react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to ITU Dict Admin Panel</header>
+    </div>
+  );
+}
+
+export default App;
