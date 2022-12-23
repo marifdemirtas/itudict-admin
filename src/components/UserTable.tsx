d8086fad143ed255378ae9a4e72db3dae16cb567
Ömer <oomeroozkan@gmail.com>
2022-12-23 19:04:55 +0300

    feat: add promote, demote and ban user features connected with backend

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -10,81 +10,11 @@ interface DataType {
   id?: string;
   username?: string;
   role?: string;
+  email?: string;
   createdAt?: Date;
   actions?: string[];
   action?: string[];
 }
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Username',
-    dataIndex: 'username',
-    key: 'username',
-  },
-  {
-    title: 'Role',
-    dataIndex: 'role',
-    key: 'role',
-  },
-  {
-    title: 'Created At',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-  },
-  {
-    title: 'Actions',
-    key: 'actions',
-    dataIndex: 'actions',
-    render: (_, { actions }) => {
-      return (
-      <>
-        {actions && 
-        actions.map((action) => {
-          let color = action ===  "Promote" ? 'geekblue' : 'green';
-          if (action === 'Ban') {
-            color = 'volcano';
-          }
-          const handleClick = (key: String) => {
-            console.log(key);   
-          }
-          return (
-            <Tag color={color} key={action} onClick={() => handleClick(action)}>
-              {action.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>)
-    },
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    dataIndex: 'action',
-    render: (_, { action }) => {
-      return (
-      <>
-        {action && action.map((action) => {
-          let color = 'black'
-          const handleClick = (key: String) => {
-            console.log(key);   
-          }
-          return (
-            <Tag color={color} key={action} onClick={() => handleClick(action)}>
-              {action.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-      )
-    },
-  }
-];
 
 export type User = {
   id: string;
@@ -99,41 +29,154 @@ export type User = {
   createdAt: Date;
 }
 
-
-const url = "http://localhost:4000/user/all";
+const getUserUrl = "http://localhost:4000/user/all";
+const banUserUrl = "http://localhost:4000/user/banUser";
+const promoteUserUrl = "http://localhost:4000/user/promoteUser";
+const demoteUserUrl = "http://localhost:4000/user/demoteUser";
 
 const UserTable = ():JSX.Element => {
-    const [tableElements, setTableElements] = useState<DataType[]>();
+  const [tableElements, setTableElements] = useState<DataType[]>();
+  const [updateTable, setUpdateTable] = useState<boolean>(false);
 
-  const { token, setToken } = useContext(CommonContext);
-
-
+  const token = localStorage.getItem("token");
   const config = {
     headers: { Authorization: `Bearer ${token}` }
   };
 
 
     useEffect(() => {
-      axios.get(url, config).then((response) => {
-        console.log("response");
-        console.log(response);
+      axios.get(getUserUrl, config).then((response) => {
         const element: DataType[] = response.data.map((user:any) => {
           const userObject: DataType = {
             id: user._id,
             username: user.username,
+            email: user.email,
             role: user.role,
             createdAt: user.createdAt,
-            actions: ["Ban", user.role === "Senior" ? "Demote" : "Promote"],
-            action: ['Delete'],
+            actions: user.role.toUpperCase() === "SENIOR" ? ["Demote"] : ["Promote"],
+            action: ['Ban'],
           }
           return userObject;
         })
-
         setTableElements(element);
-        
       });
-    }, []);
+    }, [updateTable]);
+
+    const promoteHandler = (index: any) => {
+      if (!!tableElements && tableElements.length > 0) {
+        axios.post(promoteUserUrl, {},
+          {
+            ...config,
+            params: {
+              email: tableElements[index].email
+            }
+          }).then(()=>{
+            setUpdateTable(!updateTable);
+          }
+          )
+      }
+    }
+
+    const demoteHandler = (index: any) => {
+      if (!!tableElements && tableElements.length > 0) {
+        axios.post(demoteUserUrl, {},
+          {
+            ...config,
+            params: {
+              email: tableElements[index].email
+            }
+          }).then(()=>{
+            setUpdateTable(!updateTable);
+          }
+          )
+      }    
+    }
+
+    const banHandler = (index: number) => {
+      if (!!tableElements && tableElements.length > 0) {
+        axios.post(banUserUrl, {},
+          {
+            ...config,
+            params: {
+              email: tableElements[index].email
+            }
+          }).then(()=>{
+            setUpdateTable(!updateTable);
+          }
+          )
+      }
+    }
     
+    const columns: ColumnsType<DataType> = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: 'Username',
+        dataIndex: 'username',
+        key: 'username',
+      },
+      {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+      },
+      {
+        title: 'Created At',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+      },
+      {
+        title: 'Actions',
+        key: 'actions',
+        dataIndex: 'actions',
+        render: (_, { actions }, index) => {
+          return (
+          <>
+            {actions && 
+            actions.map((action) => {
+              let color = action ===  "Promote" ? 'geekblue' : 'green';
+              const handleClick = (key: String, index: any) => {
+                if(key === "Promote") promoteHandler(index);  
+                else if(key === "Demote") demoteHandler(index);  
+              }
+              return (
+                <Tag color={color} key={action} onClick={() => handleClick(action, index)}>
+                  {action.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>)
+        },
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        dataIndex: 'action',
+        render: (_, { action }, index) => {
+          return (
+          <>
+            {action && action.map((action) => {
+              let color = 'volcano'
+              const handleClick = (index: number) => {
+                banHandler(index);  
+              }
+              return (
+                <Tag color={color} key={action} onClick={() => handleClick(index)}>
+                  {action.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>
+          )
+        },
+      }
+    ];
+    
+
     return (
     <div>
     {tableElements && <Table columns={columns} dataSource={tableElements} />}
