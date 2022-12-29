64ca261ea8b80f210bcc28107187dfe83d48c5a0
marifdemirtas <12948961+marifdemirtas@users.noreply.github.com>
2022-12-29 03:35:44 +0300

    Add: permanent backend url

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,1 @@
-export const BACKEND_API_URL = "http://localhost:4000"; // <ip_address>:<port>
+export const BACKEND_API_URL = "http://itudict-be.us-east-1.elasticbeanstalk.com"; // <ip_address>:<port>
