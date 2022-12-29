fd64feb050d13bd8e084bd001f7a15a2863914b3
Mehmet Arif Demirtaş <12948961+marifdemirtas@users.noreply.github.com>
2022-12-30 01:31:04 +0300

    Update constants.tsx

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,1 @@
-export const BACKEND_API_URL = "http://itudict-be.us-east-1.elasticbeanstalk.com"; // <ip_address>:<port>
+export const BACKEND_API_URL = "https://itudict-be.us-east-1.elasticbeanstalk.com"; // <ip_address>:<port>
